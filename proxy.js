047f79113d59b77b5dbf7f1b9ede22a2587ba31d
Jordan Taylor <jordantaylor@Jordans-MacBook-Pro-2.local>
2016-10-10 15:25:02 -0700

    Forward to correct db

--- proxy.js
+++ proxy.js
@@ -1,25 +1,62 @@
 var express = require('express');
 var app = express();
-var requiest = require('request');
+var request = require('request');
+var dbs = ['http://localhost:8888'];
+var bp = require('body-parser');
+var axios =require('axios')
 
+app.use(bp.json());
+
+
+app.use(forward)
 //requests arrive in app with the fbid appended
 app.get('/',function(req,res){
 	var hash = function(fbid){
 		return fbid % dbs.length;
 	}
 
-	var dbs = [];
+	// var db = hash(req.params.fbid);
 
-	var db = hash(req.params.fbid);
+	// request(db).then(function(response){
+	// 	res.end(response);
+	// })
 
-	request(db).then(function(response){
-		res.end(response);
-	})
+	res.end('got it')
 
 })
 
-function adddb(){
 
+
+function forward(req,res,next){
+	//console.log(req.method);
+	var url = dbs[0] + req.url;
+	var method = req.method;
+	var data = {'msg':'hello'};
+	switch(method){
+		case 'GET':
+			axios.get(url).then(function(dbres){
+				res.end(dbres.data)
+			})
+			break;
+		case 'POST':
+			axios.post(url,data).then(function(dbres){
+				res.end(dbres.data);
+			})	
+			break;
+		case 'PUT':
+			axios.post(url,data).then(function(dbres){
+				res.end(dbres.data)
+			})
+			break;
+		case 'DELETE':
+			axios.post(url,data).then(function(dbres){
+				res.end(dbres.data)
+			})
+		break;	
+		default:
+			console.log('Cant find')
+
+	}
 }
 
 function rehash(){
